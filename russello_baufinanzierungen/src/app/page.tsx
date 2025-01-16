a866df205ea358309f3b3f94b52f88c5741ccf60
Thorben Schirra <tls4@outlook.de>
2025-01-16 16:22:35 +0100

    added RDS and IAM

--- russello_baufinanzierungen/src/app/page.tsx
+++ russello_baufinanzierungen/src/app/page.tsx
@@ -1,118 +1,60 @@
 import Link from "next/link";
 import Image from "next/image";
+import Navbar from "@/app/components/Navbar";
+import Footer from "@/app/components/Footer";
 
 export default function Home() {
   return (
     <div className="">
-      <div className="m-5">
-        <div className="navbar bg-base-100">
-          <div className="navbar-start">
-            <div className="dropdown">
-              <div
-                tabIndex={0}
-                role="button"
-                className="btn btn-ghost btn-circle"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h16M4 18h7"
-                  />
-                </svg>
-              </div>
-              <ul
-                tabIndex={0}
-                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-              >
-                <li>
-                  <Link href="/">Leistungen</Link>
-                </li>
-                <li>
-                  <Link href="/">Finanzierungsrechner</Link>
-                </li>
-                <li>
-                  <Link href="/">Wir über uns</Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="navbar-center">
-            <a className="btn btn-ghost text-xl">Russello Baufinanzierungen</a>
-          </div>
-          <div className="navbar-end">
-            <button className="btn btn-ghost btn-circle">
-              <div className="indicator">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                  />
-                </svg>
-                <span className="badge badge-xs badge-primary indicator-item"></span>
-              </div>
-            </button>
-          </div>
-        </div>
-      </div>
-      <div>
-        <div className="hero bg-base-200 min-h-screen rounded-lg">
-          <div className="hero-content flex-col lg:flex-row-reverse">
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-              className="max-w-sm rounded-lg shadow-2xl"
-              alt="Testbild"
-              width="500"
-              height="500"
-            />
-            <div>
-              <h1 className="text-5xl font-bold">
-                Wir bauen auf Vertrauen - Günstige Baukredite, die ihr Zuhause
-                möglich machen
-              </h1>
-              <p className="py-6">
+      <div className="m-5 space-y-5">
+        <Navbar />
+
+        <div
+          className="hero min-h-screen"
+          style={{
+            backgroundImage:
+              "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+          }}
+        >
+          <div className="hero-overlay bg-opacity-60"></div>
+          <div className="hero-content text-neutral-content text-center">
+            <div className="max-w-md">
+              <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+              <p className="mb-5">
                 Provident cupiditate voluptatem et in. Quaerat fugiat ut
                 assumenda excepturi exercitationem quasi. In deleniti eaque aut
                 repudiandae et a id nisi.
               </p>
+              <button className="btn btn-primary">Get Started</button>
             </div>
           </div>
         </div>
       </div>
-      <div
-        className="hero min-h-screen"
-        style={{
-          backgroundImage:
-            "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
-        }}
-      >
-        <div className="hero-overlay bg-opacity-60"></div>
-        <div className="hero-content text-neutral-content text-center">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-            <p className="mb-5">
+      <div className="hero bg-base-200 min-h-screen rounded-lg">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+            className="max-w-sm rounded-lg shadow-2xl"
+            alt="Testbild"
+            width="500"
+            height="500"
+          />
+          <div>
+            <h1 className="text-5xl font-bold">
+              Wir bauen auf Vertrauen - Günstige Baukredite, die ihr Zuhause
+              möglich machen
+            </h1>
+            <p className="py-6">
               Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
               excepturi exercitationem quasi. In deleniti eaque aut repudiandae
               et a id nisi.
             </p>
-            <button className="btn btn-primary">Get Started</button>
           </div>
         </div>
+      </div>
+
+      <div>
+        <Footer />
       </div>
     </div>
   );
